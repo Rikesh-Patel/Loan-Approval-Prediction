c6fd3b552b02a146e5518499c6afbc6da4e5ccf4
Rikesh-Patel <103740388+Rikesh-Patel@users.noreply.github.com>
2022-09-05 14:52:28 -0700

    Add files via upload

---
+++ api/index.py
@@ -0,0 +1,37 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pandas as pd
+import joblib
+
+# Load the LDA model
+pipe = joblib.load('Loan-Model.pkl')
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict')
+def predict():
+        gender = request.form['gender']
+        married = request.form['married']
+        deps = request.form['dep']
+        education = request.form['education']
+        self_employed = request.form['self_employed']
+        income = float(request.form['income'])
+        co_income = float(request.form['co_income'])
+        loan = float(request.form['loan'])
+        term = float(request.form['term'])
+        history = float(request.form['history'])
+        area = request.form['area']
+        
+        data = pd.DataFrame([(gender,married,deps,education,self_employed,income,co_income,loan,term,history,area)], columns=["Gender","Married","Dependents","Education","Self_Employed","ApplicantIncome","CoapplicantIncome","LoanAmount","Loan_Amount_Term","Credit_History","Property_Area"])
+        my_prediction = int(pipe.predict(data))
+        
+        return render_template('predict.html', prediction = my_prediction)
+
+        
+
+if __name__ == '__main__':
+	app.run(debug=True)
