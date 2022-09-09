5fc32f21263ef64e569d866183f55ced82fa4853
Rikesh-Patel <103740388+Rikesh-Patel@users.noreply.github.com>
2022-09-08 19:35:45 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -18,22 +18,34 @@ def home():
 
 @app.route('/predict')
 def predict():
-        gender = request.form['gender']
-        married = request.form['married']
-        deps = request.form['dep']
-        education = request.form['education']
-        self_employed = request.form['self_employed']
-        income = float(request.form['income'])
-        co_income = float(request.form['co_income'])
-        loan = float(request.form['loan'])
-        term = float(request.form['term'])
-        history = float(request.form['history'])
-        area = request.form['area']
+#         gender = request.form['gender']
+#         married = request.form['married']
+#         deps = request.form['dep']
+#         education = request.form['education']
+#         self_employed = request.form['self_employed']
+#         income = float(request.form['income'])
+#         co_income = float(request.form['co_income'])
+#         loan = float(request.form['loan'])
+#         term = float(request.form['term'])
+#         history = float(request.form['history'])
+#         area = request.form['area']
         
-        data = pd.DataFrame([(gender,married,deps,education,self_employed,income,co_income,loan,term,history,area)], columns=["Gender","Married","Dependents","Education","Self_Employed","ApplicantIncome","CoapplicantIncome","LoanAmount","Loan_Amount_Term","Credit_History","Property_Area"])
-        my_prediction = int(pipe.predict(data))
+        # data = pd.DataFrame([(gender,married,deps,education,self_employed,income,co_income,loan,term,history,area)], columns=["Gender","Married","Dependents","Education","Self_Employed","ApplicantIncome","CoapplicantIncome","LoanAmount","Loan_Amount_Term","Credit_History","Property_Area"])
+        url= 'https://loan5.herokuapp.com/api'
+
+	import json
+	import requests
+	# sample data
+	data={'Gender':1, 'Married':1, 'Dependents':2, 'Education':0, 'Self_Employed':1,'Credit_History':0,'Property_Area':1, 'Income':1}
+	data = json.dumps(data)
+
+	# test working
+	requests.post(url, data)
+
+	send_req = requests.post(url, data)
+	#my_prediction = int(pipe.predict(data))
         
-        return render_template('predict.html', prediction = my_prediction)
+        return render_template('predict.html', prediction = send_req.json())
 
         
 
