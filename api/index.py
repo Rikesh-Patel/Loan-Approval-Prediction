173980e3a6bac49cec916d54a986fb50bd4fd9f2
Rikesh-Patel <103740388+Rikesh-Patel@users.noreply.github.com>
2022-09-08 21:16:19 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -37,7 +37,7 @@ def predict():
         requests.post(url, data) 
         send_req = requests.post(url, data) 
         #my_prediction = int(pipe.predict(data))
-        return render_template('predict.html', prediction = send_req.json())
+        return render_template('predict.html', prediction = send_req)
         
 if __name__ == '__main__':
 	app.run(debug=True)
