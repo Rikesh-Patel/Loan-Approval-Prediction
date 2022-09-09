22dc70b76df8568425f5219346987d23a5ffa4f7
Rikesh-Patel <103740388+Rikesh-Patel@users.noreply.github.com>
2022-09-08 23:39:05 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -28,16 +28,16 @@ def predict():
 #         area = request.form['area']
         
         # data = pd.DataFrame([(gender,married,deps,education,self_employed,income,co_income,loan,term,history,area)], columns=["Gender","Married","Dependents","Education","Self_Employed","ApplicantIncome","CoapplicantIncome","LoanAmount","Loan_Amount_Term","Credit_History","Property_Area"])
-        url= 'https://loan5.herokuapp.com/api'
-        import json 
-        import requests 
-        # sample data
-        data={'Gender':1, 'Married':1, 'Dependents':2, 'Education':0, 'Self_Employed':1,'Credit_History':0,'Property_Area':1, 'Income':1} 
-        data = json.dumps(data) 
-        # test working 
-        requests.post(url, data) 
-        send_req = requests.post(url, data) 
-        #my_prediction = int(pipe.predict(data))
+#         url= 'https://loan5.herokuapp.com/api'
+#         import json 
+#         import requests 
+#         # sample data
+#         data={'Gender':1, 'Married':1, 'Dependents':2, 'Education':0, 'Self_Employed':1,'Credit_History':0,'Property_Area':1, 'Income':1} 
+#         data = json.dumps(data) 
+#         # test working 
+#         requests.post(url, data) 
+#         send_req = requests.post(url, data) 
+#         #my_prediction = int(pipe.predict(data))
         return render_template('predict.html', prediction = 'Y')
         
 if __name__ == '__main__':
